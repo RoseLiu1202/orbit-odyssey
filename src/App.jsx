7adfd6acfec2031827c52832c6f35862e047c4e5
Rose Liu <liu.rose@wustl.edu>
2024-12-16 13:55:58 -0600

    update

--- src/App.jsx
+++ src/App.jsx
@@ -145,7 +145,7 @@ const App = () => {
 
                 {/* Process Book Button */}
                 <div className="process-book-link">
-                    <a href="public/processbook.html" target="_blank" rel="noopener noreferrer">
+                    <a href="/processbook.html" target="_blank" rel="noopener noreferrer">
                         Process Book
                     </a>
                 </div>
